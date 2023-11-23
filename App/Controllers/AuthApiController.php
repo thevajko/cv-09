48affe0630462cfc6f4f707126117ca4304de013
hrkut <hrkut@kst.uniza.sk>
2023-11-23 13:35:41 +0100

    komplet API pre logins

--- App/Controllers/AuthApiController.php
+++ App/Controllers/AuthApiController.php
@@ -101,6 +101,10 @@ class AuthApiController extends AControllerBase {
      * @throws HTTPException 401 Unauthorized -  if user is not logged in
      */
     public function activeUsers() {
-        throw new HTTPException(501,"Not Implemented");
+       if ($this->app->getAuth()->isLogged()) {
+           return $this->json(Login::getAllActive());
+       } else {
+           throw new HTTPException('401', 'Not authorized');
+       }
     }
 }
